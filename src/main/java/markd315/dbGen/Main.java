d9394fa44355939f392e32166c27cddec1f2e90c
markd315 <markd315@gmail.com>
2018-12-22 14:25:07 -0500

    Repository generation works

--- src/main/java/markd315/dbGen/Main.java
+++ src/main/java/markd315/dbGen/Main.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args, Object contents) {
+    public static void main(String[] args) {
         //traverse folder: src/main/java/io/swagger/model and read class names into
         Set<String> classNames = new HashSet<>();
         classNames.add("Pet");
@@ -20,8 +20,8 @@ public class Main {
         //mongo/pom.xml -> pom.xml
         for(String classname : classNames){
             String fileContents = Const.getClassRepository().replaceAll("\\{\\{Class\\}\\}", classname).replaceAll("\\{\\{class\\}\\}",classname.toLowerCase());
-            System.out.println(fileContents);
-            fileWrite(fileContents, "src/main/java/io/swagger/repository/"+classname+".java");
+            //System.out.println(fileContents);
+            fileWrite(fileContents, "src/main/java/io/swagger/repository/"+classname+"Repository.java");
         }
     }
 
